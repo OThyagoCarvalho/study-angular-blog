4f91c7cea0269800f2908a9bf6778f9269bf353f
Thyago Carvalho <othyagocarvalho@outlook.com>
2023-01-20 11:08:35 -0300

    create components structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { BlogTitleComponent } from './components/blog-title/blog-title.component';
+import { BigPostComponent } from './components/big-post/big-post.component';
+import { SmallPostComponent } from './components/small-post/small-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    BlogTitleComponent,
+    BigPostComponent,
+    SmallPostComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
