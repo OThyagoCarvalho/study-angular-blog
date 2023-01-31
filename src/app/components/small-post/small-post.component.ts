4271415d49fd8bb0e4a30a6ab21590d6ec576a1e
Thyago Carvalho <othyagocarvalho@outlook.com>
2023-01-30 22:29:09 -0300

    create small post component

--- src/app/components/small-post/small-post.component.ts
+++ src/app/components/small-post/small-post.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-small-post',
   templateUrl: './small-post.component.html',
-  styleUrls: ['./small-post.component.css']
+  styleUrls: ['./small-post.component.css'],
 })
 export class SmallPostComponent {
+  constructor() {}
+  
+  //normalize title
+  normalizeTitle(title: string): string {
+    return title.replace(/ /g, '-').toLowerCase();
+  }
 
+  //hide titles bigger than 50 characters
+  hideTitle(title: string): string {
+    if (title.length > 40) {
+      return title.slice(0, 40) + '...';
+    }
+    return title;
+  }
 }
