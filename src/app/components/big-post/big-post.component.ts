be77c3717cdce4c003c38d8f5ce1cf4d72d7a19b
Thyago Carvalho <othyagocarvalho@outlook.com>
2023-01-31 16:40:51 -0300

    feat: add property bind to bigpost

--- src/app/components/big-post/big-post.component.ts
+++ src/app/components/big-post/big-post.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-post',
   templateUrl: './big-post.component.html',
-  styleUrls: ['./big-post.component.css']
+  styleUrls: ['./big-post.component.css'],
 })
-export class BigPostComponent {
+export class BigPostComponent implements OnInit {
+  @Input()
+  postCover: string = '';
+  @Input()
+  postTitle: string = '';
+  @Input()
+  postDescription: string = '';
+
+  constructor() {}
+  ngOnInit(): void {}
 
 }
