43d535a26f0f429affce93939f0445fb47d19d45
Thyago Carvalho <othyagocarvalho@outlook.com>
2023-02-04 14:24:18 -0300

    create mock data for posts

---
+++ src/app/data/mock-data.ts
@@ -0,0 +1,30 @@
+export const mock_data = [
+  {
+    id: '001',
+    cover_url: 'https://www.apple.com/br/iphone-14/images/overview/hero/hero_all_colors__dvlwc1u257qu_large.jpg',
+    alt_text: 'iphone 14',
+    post_title: 'O iPhone 14 está aqui',
+    post_description: 'A cara do iPhone vai ficar a sua cara com a nova Tela Bloqueada do iOS 16. Selecione uma parte da foto para deixar em primeiro plano. Mude a fonte. Confira seus círculos de Atividade. E veja atualizações ao vivo dos seus apps favoritos.'
+  },
+  {
+    id: '002',
+    cover_url: 'https://www.apple.com/v/iphone-14/d/images/overview/camera/camera_hero_endframe__cfnnwvb6czte_large.jpg',
+    alt_text: 'iphone camera',
+    post_title: 'Câmera de altíssima qualidade',
+    post_description: 'Detalhes detalhistas. Cores coloridas. Fotos fotogênicas. Com a nova câmera grande‑angular de nível profissional e melhorias no processamento de imagem, você faz fotos ainda mais espetaculares em qualquer cenário. Prepare‑se para ver a magia acontecer em pouca luz.'
+  },
+  {
+    id: '003',
+    cover_url: 'https://www.apple.com/br/iphone-14/images/overview/hero/hero_all_colors__dvlwc1u257qu_large.jpg',
+    alt_text: 'iphone 14',
+    post_title: 'O iPhone 14 está aqui',
+    post_description: 'A cara do iPhone vai ficar a sua cara com a nova Tela Bloqueada do iOS 16. Selecione uma parte da foto para deixar em primeiro plano. Mude a fonte. Confira seus círculos de Atividade. E veja atualizações ao vivo dos seus apps favoritos.'
+  },
+  {
+    id: '004',
+    cover_url: 'https://www.apple.com/v/iphone-14/d/images/overview/camera/camera_hero_endframe__cfnnwvb6czte_large.jpg',
+    alt_text: 'iphone camera',
+    post_title: 'Câmera de altíssima qualidade',
+    post_description: 'Detalhes detalhistas. Cores coloridas. Fotos fotogênicas. Com a nova câmera grande‑angular de nível profissional e melhorias no processamento de imagem, você faz fotos ainda mais espetaculares em qualquer cenário. Prepare‑se para ver a magia acontecer em pouca luz.'
+  } 
+]
